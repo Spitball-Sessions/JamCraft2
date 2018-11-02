20d897891322a8348ea78f17d936000ac58bbb67
Koholos <Koholos@yahoo.com>
2018-11-01 22:35:21 -0400

    Base Roguelike Program
    *@ moves
    * Basic map
    
    TODO:
    
    * Modify Map
    * Add Enemies
    * Add Combat
    * Add Stats/Score
    
    Long-Term:
    * Menu
    * Difficulty Settings
    * Instructions

--- Core.py
+++ Core.py
@@ -1,11 +1,24 @@
 import tdl
 from render_func import clear_all, render_all
+from map_utils import make_map
 from handlers import handle_keys
 from entities import Entity
 
 def main():
     screen_width = 80
     screen_height = 50
+
+    map_width = 65
+    map_height = 50
+
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
+    colors = {
+        "d_wall": 0xCCCCCC,
+        "d_ground": 0x1C1C1C
+    }
 
     player = Entity(int(screen_width  /2), int(screen_height /2), '@', 0x0DE6F0)
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", 0xFFFFFF)
@@ -17,8 +30,11 @@ def main():
     root_console = tdl.init(screen_width,screen_height, title = "E=Quality")
     con = tdl.Console(screen_width,screen_height)
 
+    game_map = tdl.map.Map(map_width, map_height)
+    make_map(game_map, max_rooms, room_min_size, room_max_size, map_width, map_height, player)
+
     while not tdl.event.is_window_closed():
-        render_all(con, entities, root_console, screen_width, screen_height)
+        render_all(con, entities, game_map, root_console, screen_width, screen_height, colors)
         tdl.flush()
 
         clear_all(con, entities)
@@ -42,7 +58,8 @@ def main():
 
         if move:
             dx, dy = move
-            player.move(dx,dy)
+            if game_map.walkable[player.x + dx, player.y + dy]:
+               player.move(dx,dy)
 
         if exit:
             return True
@@ -54,6 +71,7 @@ def main():
 
 
 if __name__ == '__main__':
+    '''
     print("Do you want to play easy or hard mode?")
     choice = input()
     if choice == "easy":
@@ -62,5 +80,5 @@ if __name__ == '__main__':
         print("hard")
     else:
         print("no input")
-
+    '''
     main()
---
+++ map_utils.py
@@ -0,0 +1,81 @@
+from random import randint
+
+
+class Rect:
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.x2 = x + w
+        self.y1 = y
+        self.y2 = y + h
+
+    def center(self):
+        center_x = int((self.x1 + self.x2) / 2)
+        center_y = int((self.y1 + self.y2) / 2)
+        return (center_x, center_y)
+
+    def intersect(self, other):
+        # returns true if this rectangle intersects with another one
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
+
+def create_room(game_map, room):
+    # check tiles to make passable
+    for x in range(room.x1 + 1, room.x2):
+        for y in range(room.y1 + 1, room.y2):
+            game_map.walkable[x,y] = True
+            game_map.transparent[x,y] = True
+
+def create_h_tunnel(game_map, x1, x2, y):
+    for x in range(min(x1, x2), max(x1, x2) + 1):
+        game_map.walkable[x, y] = True
+        game_map.transparent[x,y] = True
+
+def create_v_tunnel(game_map, y1, y2, x):
+    for y in range(min(y1, y2), max(y1, y2) + 1):
+        game_map.walkable[x, y] = True
+        game_map.transparent[x,y] = True
+
+
+def make_map(game_map, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
+    rooms = []
+    num_rooms = 0
+
+    for r in range(max_rooms):
+        # random width & height
+        w = randint(room_min_size, room_max_size)
+        h = randint(room_min_size, room_max_size)
+        # random position
+        x = randint(0, map_width - w - 1)
+        y = randint(0, map_height - h - 1)
+
+        new_room = Rect(x, y, w, h)
+
+        for other_room in rooms:
+            if new_room.intersect(other_room):
+                break
+
+        else:
+            # this would be the valid rooms
+            create_room(game_map, new_room)
+            (new_x, new_y) = new_room.center()
+
+            if num_rooms == 0:
+                player.x = new_x
+                player.y = new_y
+
+            else:
+                (prev_x, prev_y) = rooms[num_rooms - 1].center()
+
+                # flip a coin (random number that is either 0 or 1)
+                if randint(0, 1) == 1:
+                    # first move horizontally, then vertically
+                    create_h_tunnel(game_map, prev_x, new_x, prev_y)
+                    create_v_tunnel(game_map, prev_y, new_y, new_x)
+                else:
+                    # first move vertically, then horizontally
+                    create_v_tunnel(game_map, prev_y, new_y, prev_x)
+                    create_h_tunnel(game_map, prev_x, new_x, new_y)
+
+
+            rooms.append(new_room)
+            num_rooms += 1
